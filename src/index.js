07deb78a9c689331c48d4835eca259621bebe662
Islam Ahmed <142545372+SoftEng-Islam@users.noreply.github.com>
2023-12-26 04:11:23 +0200

    update

--- src/index.js
+++ src/index.js
@@ -2,14 +2,28 @@ const express = require('express');
 const app = express();
 const PORT = 3001;
 
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
+
 app.listen(PORT, ()=> {
     console.log(`http://localhost:${PORT}`);
 });
 
-app.get("/", (req, res) => {
-    res.send({
+const books = [
+    {
         item: 'harry potter'
-    })
+    },
+    {
+        item: 'LordoftheFlies'
+    },
+];
+
+app.get("/", (req, res) => {
+    res.send(books);
 });
 
-app.post("")
+app.post("/", (req, res)=> {
+    console.log(req.body);
+    // res.send(201);
+    res.status(201).send();
+})
