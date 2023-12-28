2a46b5a0a5ed087aa79eeb001652bb3154d75a0c
Islam Ahmed <142545372+SoftEng-Islam@users.noreply.github.com>
2023-12-28 02:54:28 +0200

    update

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 const express = require('express');
+const groceriesRoute = require('./routes/groceries');
 const app = express();
 const PORT = 3001;
+
+
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -10,40 +13,9 @@ app.use((req,res,next)=>{
     next();
 });
 
+// Prefix with api word
+app.use('/api', groceriesRoute);
 
 app.listen(PORT, () => {
     console.log(`http://localhost:${PORT}`);
-});
-
-const books = [
-    {
-        item: 'harry potter'
-    },
-    {
-        item: 'LordoftheFlies'
-    },
-];
-
-app.get("/books",
-    (req, res, next) => {
-        console.log('before handling request.');
-        next();
-    },
-    (req, res, next) => {
-        res.send(books);
-        next();
-    },
-    (req, res, next) => {
-        console.log('Finished Executing GET Request.');
-        next()
-    },
-    (req, res, next) =>{
-        console.log('The end');
-    }
-);
-
-app.post("/books", (req, res, next) => {
-    console.log(req.body);
-    books.push(req.body);
-    res.status(201).send();
 });
---
+++ src/routes/groceries.js
@@ -0,0 +1,55 @@
+const {Router} = require('express');
+
+const router = Router();
+
+
+const groceryList = [
+    {
+        item: 'harry potter'
+    },
+    {
+        item: 'LordoftheFlies'
+    },
+    {
+        item: 'milk'
+    },
+];
+
+
+router.get("/groceries",
+    (req, res, next) => {
+        console.log('before handling request.');
+        next();
+    },
+    (req, res, next) => {
+        res.send(groceryList);
+        next();
+    },
+    (req, res, next) => {
+        console.log('Finished Executing GET Request.');
+        next()
+    },
+    (req, res, next) =>{
+        console.log('The end');
+    }
+);
+
+router.get('/groceries/:item', (req, res) =>{
+    const {item} = req.params;
+    const groceryItem = groceryList.find((g) => g.item === item);
+    console.log(item);
+    res.send(groceryItem);
+    res.status(200).send();
+});
+
+
+
+// ----------------------
+// POST Method
+router.post("/groceries", (req, res, next) => {
+    console.log(req.body);
+    books.push(req.body);
+    res.status(201).send();
+});
+
+module.exports = router;
