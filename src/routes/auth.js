3c82027146c3f2f4a8ad7882d9cc46e43267b24a
Islam Ahmed <142545372+SoftEng-Islam@users.noreply.github.com>
2023-12-31 10:06:42 +0200

    update

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -1,41 +1,50 @@
 const  {Router} = require('express');
 const User = require('../database/schemas/User');
 const router = Router();
-const {hashPassword} = require('../utils/helpers');
+const {hashPassword, comparePassword} = require('../utils/helpers');
 
-router.post('/auth/login', (req, res) => {
+router.post('/auth/login', async (req, res) => {
     const {username, password} = req.body;
+    if(!username || !password) return res.sendStatus(400);
+    const userDB = await User.findOne({username});
+    if(!userDB) return res.sendStatus(401);
 
-
-
-
-    if(username && password) {
-        if(req.session.user) {
-            res.send('You are already logged in!');
-        } else {
-            req.session.user = {
-                username
-            };
-            res.send(req.session);
-        }
+    const isValid = comparePassword(password, userDB.password);
+    if(isValid) {
+        console.log('Authenticated Successfully!');
+        req.session.user = userDB;
+        return res.sendStatus(200);
     } else {
-        res.send(401);
+        console.log('failed to Authenticate');
+        return res.sendStatus(401);
     }
+
+    // if(username && password) {
+    //     if(req.session.user) {
+    //         res.send('You are already logged in!');
+    //     } else {
+    //         req.session.user = {
+    //             username
+    //         };
+    //         res.send(req.session);
+    //     }
+    // } else {
+    //     res.send(401);
+    // }
 })
 
 router.post('/auth/register', async (req,res) =>{
     const {username, email} = req.body;
     const userDB = await User.findOne({$or:[{username}, {email}]});
     if(userDB) {
-        res.status(400).send({msg: 'User already exists!'});
+        res.sendStatus(400).send({msg: 'User already exists!'});
     } else {
         const password = hashPassword(req.body.password);
         console.log(password);
         const newUser = await User.create({username,password,email});
         newUser.save();
-        res.status(201).send();
+        res.sendStatus(201);
     }
 })
-
 
 module.exports = router;
