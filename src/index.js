1fb08f28fe8960e1be8359a7375af02cd3fc69f7
Islam Ahmed <142545372+SoftEng-Islam@users.noreply.github.com>
2024-01-05 05:57:06 +0200

    update

--- src/index.js
+++ src/index.js
@@ -4,7 +4,9 @@ const express = require('express');
 const session = require('express-session');
 const cookieParser = require('cookie-parser')
 const passport = require('passport');
-require('./strategies/local');
+
+
+
 
 // Routes
 const groceriesRoute = require('./routes/groceries');
@@ -17,7 +19,9 @@ require('./database/index');
 const app = express();
 const PORT = 3001;
 
-
+app.use(passport.initialize());
+app.use(passport.session());
+require('./strategies/local');
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -46,10 +50,10 @@ app.use('/api', groceriesRoute);
 app.use('/api', bookMarket);
 app.use('/api', authRoute);
 
-// app.use((req,res, next)=>{
-//     if(req.session.user) next();
-//     else res.status(401).send();
-// })
+app.use((req,res, next)=>{
+    if(req.session.user) next();
+    else res.status(401).send();
+})
 
 
 app.listen(PORT, () => {
